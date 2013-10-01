b168d4a136bfb1224e5cc6dc92824a28754b1835
pennpanda <pennpanda@pennpanda-OptiPlex-990.(none)>
2013-10-01 15:11:15 -0400

    add rtas14 folder for rtas14 paper add rtxen folder for rtxen's scripts

---
+++ rtxen/dependency_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install bcc bin86 gawk bridge-utils iproute libcurl3 libcurl4-openssl-dev bzip2 module-init-tools transfig tgif 
+sudo apt-get install texinfo texlive-latex-base texlive-latex-recommended texlive-fonts-extra texlive-fonts-recommended pciutils-dev mercurial
+sudo apt-get install make gcc libc6-dev zlib1g-dev python python-dev python-twisted libncurses5-dev patch libvncserver-dev libsdl-dev libjpeg62-dev
+sudo apt-get install iasl libbz2-dev e2fslibs-dev git-core uuid-dev ocaml ocaml-findlib libx11-dev bison flex xz-utils libyajl-dev
+sudo apt-get install gettext
